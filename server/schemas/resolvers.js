c909a284af90160dd7784828f4993b817c78a65d
Justin Hoch <justinhoch22@gmail.com>
2021-09-14 16:15:36 -0500

    Added login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,20 @@ const resolvers = {
     },
 
     Mutation: {
-        
+        login: async (parent, { body }) => {
+            const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+            if (!user) {
+                return res.status(400).json({ message: "Can't find this user" });
+            }
+
+            const correctPw = await user.isCorrectPassword(body.password);
+
+            if (!correctPw) {
+                return res.status(400).json({ message: 'Wrong password!' });
+            }
+            const token = signToken(user);
+            res.json({ token, user });
+        }
     }
 }
 
