5d9dbe38aed06ba15daa506b842369fe3ff37d04
Justin Hoch <justinhoch22@gmail.com>
2021-09-14 16:16:45 -0500

    Added removeBook mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -46,6 +46,17 @@ const resolvers = {
                 console.log(err);
                 return res.status(400).json(err);
             }
+        },
+        removeBook: async (parent, { user, params }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+            );
+            if (!updatedUser) {
+                return res.status(404).json({ message: "Couldn't find user with this id!" });
+            }
+            return res.json(updatedUser);
         }
     }
 }
